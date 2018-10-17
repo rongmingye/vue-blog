9b1641bdca42dbf5369055743d0627663d5a96d4
rongmingye <1164898915@qq.com>
2018-10-17 16:01:59 +0800

    更改启动服务文件

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(express.static(__dirname));
 router(app);
 routerManage(app);
 
+var developState = "/public"; //开发时是"/public", 打包后用"/build"
+
+// 所有的请求都回到index页面
+app.get('/', function(req, res){
+    res.sendFile(__dirname+developState+'/index.html');
+});
+
 var server = app.listen(8081, '0.0.0.0', function(){
 	var host = server.address().address;
     var port = server.address().port;
