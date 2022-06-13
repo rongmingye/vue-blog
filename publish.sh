b8b09baaffde8a2a3ac6ecfb2c863eeff6d55ade
rongmingye <11164898915@qq.com>
2022-06-13 16:33:25 +0800

    build: 前端发版命令

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "publish start"
+
+# 切换到生产分支
+git checkout master
+
+# 拉取最新代码
+git pull
+
+# 打包
+npm run build
+
+# 将打包目录复制到线上目录
+cp -r ./dist/ /usr/project_online/fe/vue-blog/
+
+echo "publish success"
